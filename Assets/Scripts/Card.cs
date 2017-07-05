d58bd3c163d2dce8bd51fdb24cdd7774f5290404
BatManBatManBatMan <imbatmanyear1@gmail.com>
2017-07-05 14:21:36 +0200

    Project beginning

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public static float rotChangePoint = 90;
+    private static string[] frontsNames = { "Silvio0", "Silvio1", "Silvio2", "Silvio3", "Silvio4", "Silvio5", "Silvio6", "Silvio7", "Silvio8", "Silvio9", "Silvio10", "Silvio11", "Silvio12", "Silvio13", "Silvio14", "Silvio15", "Silvio16", "Silvio17", "Silvio18", "Silvio19" };
+
+    private string front;
+    public string back;
+
+    public static Dictionary<GameObject, Card> cards = new Dictionary<GameObject, Card>();
+
+    public UISprite cardSprite;
+
+    public TweenRotation tweenRot;
+    public TweenPosition tweenPos;
+
+    private void AddMe()
+    {
+        cards.Add(gameObject, this);
+    }
+
+    public void ChangeFace()
+    {
+        while (gameObject.transform.rotation.eulerAngles.y < rotChangePoint)
+        {
+            return;
+        }
+        if (cardSprite.spriteName == front)
+            cardSprite.spriteName = back;
+        else if (cardSprite.spriteName == back)
+            cardSprite.spriteName = front;
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask selectableCardMask;
+    private RaycastHit hitInfo;
+
+    public void SelectCard()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if(Physics.Raycast(ray, out hitInfo, selectableCardMask))
+        {
+            
+        }
+    }
+
+    public void FlipCard(Card card)
+    {
+        card.tweenRot.from = transform.position;
+        card.tweenRot.to = new Vector3(transform.position.x, rotChangePoint * 2, transform.position.z);
+        card.tweenRot.PlayForward();
+        card.ChangeFace();
+    }
+
+    public void MoveCard (Card card, Vector3 to)
+    {
+        card.tweenPos.from = card.gameObject.transform.position;
+        card.tweenPos.to = to;
+        card.tweenPos.PlayForward();
+    }
+
+}
